c208b2f5c6a948fa9de5e072ec604b23ca9be03f
Joydeep Sen Sarma <jssarma@qubole.com>
2013-07-09 10:16:45 +0200

    release 1.0.3-beta - add min version for requests dependency

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 import os
 from setuptools import setup
 
-INSTALL_REQUIRES = ['python_cjson', 'requests']
+INSTALL_REQUIRES = ['python_cjson', 'requests >=1.2.3']
 
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
     name = "qds_sdk",
-    version = "1.0.2-beta",
+    version = "1.0.3-beta",
     author = "Qubole",
     author_email = "dev@qubole.com",
     description = ("Python SDK for coding to the Qubole Data Service API"),
