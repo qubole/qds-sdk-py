20c813d5c08125d86e2cdddfe34fc2cb5e5ffecf
Vikram Agrawal <vagrawal@qubole.com>
2014-02-06 15:16:16 +0530

    updated version number

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(fname):
 
 setup(
     name = "qds_sdk",
-    version = "1.0.9_beta",
+    version = "1.0.10_beta",
     author = "Qubole",
     author_email = "dev@qubole.com",
     description = ("Python SDK for coding to the Qubole Data Service API"),
