c39b99fddf8ea8e0c033550e23189a66c763a1f2
Rohit Agarwal <rohita@qubole.com>
2014-03-25 16:04:55 +0530

    Lower required argparse version.
    The newer argparse versions require --allow-external flag to be passed to pip
    as they are hosted on googlecode and not on pypi. This may break some
    installation scripts.

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ from setuptools import setup
 
 INSTALL_REQUIRES = ['python_cjson', 'requests >=1.0.3', 'boto >=2.1.1']
 if sys.version_info < (2, 7, 0):
-    INSTALL_REQUIRES.append('argparse>=1.2.1')
+    INSTALL_REQUIRES.append('argparse>=1.1')
 
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
     name = "qds_sdk",
-    version = "1.0.11b1",
+    version = "1.0.12b1",
     author = "Qubole",
     author_email = "dev@qubole.com",
     description = ("Python SDK for coding to the Qubole Data Service API"),
