8ec65137efcf1f8cf37923b916e7496e10027edc
Rohit Agarwal <mindprince@gmail.com>
2014-04-29 13:48:10 +0530

    Bump version.
    The new version is no longer beta.
    No one treated the SDK as beta anyways and it was a pain to tell everyone to specify --pre while installing.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def read(fname):
 
 setup(
     name = "qds_sdk",
-    version = "1.0.15b1",
+    version = "1.0.16",
     author = "Qubole",
     author_email = "dev@qubole.com",
     description = ("Python SDK for coding to the Qubole Data Service API"),
