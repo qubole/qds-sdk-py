9687f9df68ef8c6fc34b84ac3d31ba085af39566
Rohit Agarwal <rohita@qubole.com>
2014-06-06 11:57:01 +0530

    Change version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def read(fname):
 
 setup(
     name = "qds_sdk",
-    version = "1.1.2",
+    version = "1.1.2rc1",
     author = "Qubole",
     author_email = "dev@qubole.com",
     description = ("Python SDK for coding to the Qubole Data Service API"),
