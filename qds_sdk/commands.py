qubole/qds-sdk-py

commit e22b3ee6c789d55b86ea39520710802de023a323

HadoopCommand should use the same parsing pattern as other commands. Add --notify option to hadoopcmd run|submit

qds_sdk/commands.py
@@ -294,7 +294,15 @@ class PrestoCommand(Command):
 
 class HadoopCommand(Command):
     subcmdlist = ["jar", "s3distcp", "streaming"]
-    usage = "hadoopcmd <submit|run> <%s> <arg1> [arg2] ..." % " | ".join(subcmdlist)
+    usage = "hadoopcmd <submit|run> [options] <%s> <arg1> [arg2] ..." % "|".join(subcmdlist)
+
+    optparser = GentleOptionParser(usage=usage)
+    optparser.add_option("--cluster-label", dest="label",
+                         help="the label of the cluster to run the command on")
+
+    optparser.add_option("--notify", action="store_true", dest="can_notify",
+                         default=False, help="sends an email on command completion")
+
 
     @classmethod
     def parse(cls, args):
@@ -313,14 +321,15 @@ class HadoopCommand(Command):
         """
         parsed = {}
 
-        if len(args) >= 1 and args[0] == "-h":
-            sys.stderr.write(cls.usage + "\n")
+        try:
+            (options, args) = cls.optparser.parse_args(args)
+        except OptionParsingError as e:
+            raise ParseError(e.msg, cls.optparser.format_help())
+        except OptionParsingExit as e:
             return None
 
-        if len(args) >= 2 and args[0] == "--cluster-label":
-            parsed['label'] = args[1]
-            args.pop(0)
-            args.pop(0)
+        parsed['label'] = options.label
+        parsed['can_notify'] = options.can_notify
 
         if len(args) < 2:
             raise ParseError("Need at least two arguments", cls.usage)

commit 124928b181dd2c3a024a4d3a20127f2e74fd7251

Fix bug in hadoopcmd parsing introduced in e22b3ee.
OptionParser introduced in e22b3ee was preventing commands like:
qds.py hadoopcmd submit s3distcp --src source --dest destincation
from working as they were erroring out because --src etc. are not
known to optparser.

qds_sdk/commands.py
@@ -319,6 +319,7 @@ class HadoopCommand(Command):
     optparser.add_option("--notify", action="store_true", dest="can_notify",
                          default=False, help="sends an email on command completion")
 
+    optparser.disable_interspersed_args()
 
     @classmethod
     def parse(cls, args):