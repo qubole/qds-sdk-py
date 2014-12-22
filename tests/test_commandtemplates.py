c7ad323942c2026528a73e6b32db85e843023c01
rohitpruthi95 <rohitpruthi95@gmail.com>
2014-12-22 15:21:32 +0530

    Few more test cases added

--- tests/test_commandtemplates.py
+++ tests/test_commandtemplates.py
@@ -5,7 +5,7 @@ if sys.version_info > (2, 7, 0):
     import unittest
 else:
     import unittest2 as unittest
-from mock import Mock
+from mock import *
 import tempfile
 sys.path.append(os.path.join(os.path.dirname(__file__), '../bin'))
 import qds
@@ -13,6 +13,13 @@ import qds_sdk
 from qds_sdk.connection import Connection
 from test_base import print_command
 from test_base import QdsCliTestCase
+
+
+def view_templates_side_effect(*args, **kwargs):
+    if args[1] == "command_templates/123":
+      return {'id':'123'}
+    else:
+      return {"actions":[]}
 
 
 class TestCommandTemplate(QdsCliTestCase):
@@ -38,23 +45,25 @@ class TestCommandTemplate(QdsCliTestCase):
         Connection._api_call.assert_called_with("GET", "command_templates/123", params=None)
 
 
-    # Not sure why the below test cases are not working here, although the command works otherwise
+    def test_remove(self):
+        sys.argv = ['qds.py', 'commandtemplates', 'remove', '123']
+        print_command()
+        Connection._api_call = Mock()
+        Connection._api_call.side_effect = view_templates_side_effect
+        qds.main()
+        Connection._api_call.assert_has_calls([call("GET", "command_templates/123", params=None),
+            call("PUT", "command_templates/123/remove", {})])
 
-    # def test_remove(self):
-    #     sys.argv = ['qds.py', 'commandtemplates', 'remove', '123']
-    #     print_command()
-    #     Connection._api_call = Mock(return_value={})
-    #     qds.main()
-    #     Connection._api_call.assert_called_with("PUT", "command_templates/123/remove", params=None)
+    def test_run(self):
+        sys.argv = ['qds.py', 'commandtemplates', 'run', '123',
+         '--input_vars', "table_name='doctors'"]
+        print_command()
+        Connection._api_call = Mock()
+        Connection._api_call.side_effect = view_templates_side_effect
+        qds.main()
+        Connection._api_call.assert_has_calls([call("GET", "command_templates/123", params=None),
+            call("POST", "command_templates/123/run", {'input_vars': [{'table_name':"'doctors'"}]})])
 
-    # def test_run(self):
-    #     sys.argv = ['qds.py', 'commandtemplates', 'run', '123',
-    #      '--input_vars', "table_name='doctors'"]
-    #     print_command()
-    #     Connection._api_call = Mock(return_value={})
-    #     qds.main()
-    #     Connection._api_call.assert_called_with("POST", "command_templates/123/run",
-    #         {'input_vars': [{'name':'table_name','default_value':"'doctors'"}]})
 
 
 if __name__ == '__main__':
