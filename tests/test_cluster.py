cffada78a79377084d45ac65bfc2025b96e1bea7
Adesh <adesh.rao.9993@gmail.com>
2015-04-07 03:14:31 -0400

    test fix quotes

--- tests/test_cluster.py
+++ tests/test_cluster.py
@@ -1485,14 +1485,14 @@ class TestClusterHbaseSnapshot(QdsCliTestCase):
         print_command()
         Connection._api_call = Mock(return_value={})
         qds.main()
-        Connection._api_call.assert_called_with('POST', 'clusters/1234/snapshot', {'parameters': ' {"s3_location":"myString", "backup_type":"full"}'})
+        Connection._api_call.assert_called_with('POST', 'clusters/1234/snapshot', {'parameters':  {'s3_location':'myString', 'backup_type':'full'}})
 
     def test_restore_point(self):
         sys.argv = ['qds.py', 'cluster', 'restore_point', '--label', '1234', '--parameters', ' {"s3_location":"myString", "backup_id":"abcd", "table_names":"tablename"}']
         print_command()
         Connection._api_call = Mock(return_value={})
         qds.main()
-        Connection._api_call.assert_called_with('POST', 'clusters/1234/restore_point', {'parameters': ' {"s3_location":"myString", "backup_id":"abcd", "table_names":"tablename"}'})
+        Connection._api_call.assert_called_with('POST', 'clusters/1234/restore_point', {'parameters':  {'s3_location':'myString', 'backup_id':'abcd', 'table_names':'tablename'}})
 
 if __name__ == '__main__':
     unittest.main()
