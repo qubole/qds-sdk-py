629822ff5136dff271a146e7361d91ce75e7b0e6
Rohit Agarwal <rohita@qubole.com>
2015-10-29 14:56:57 -0700

    PEP8 improvements.

--- tests/test_base.py
+++ tests/test_base.py
@@ -1,10 +1,12 @@
 from __future__ import print_function
 import sys
 import os
+
 if sys.version_info > (2, 7, 0):
     import unittest
 else:
     import unittest2 as unittest
+
 
 def print_command():
     print()
