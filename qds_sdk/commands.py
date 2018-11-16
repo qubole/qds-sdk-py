32fec05883ebe45d8e4023cf83b22d41d374391a
Joy Lal Chattaraj <joylal4896@gmail.com>
2018-11-16 12:37:29 +0530

    change to db-schema and default None

--- qds_sdk/commands.py
+++ qds_sdk/commands.py
@@ -987,8 +987,8 @@ class DbExportCommand(Command):
     optparser = GentleOptionParser(usage=usage)
     optparser.add_option("-m", "--mode", dest="mode",
                          help="Can be 1 for Hive export or 2 for HDFS/S3 export")
-    optparser.add_option("--schema", help="Hive schema name assumed to be 'default' if not specified",
-                              default="default", dest="schema")
+    optparser.add_option("--db-schema", help="Db schema name assumed accordingly by database if not specified",
+                         default=None, dest="schema")
     optparser.add_option("--hive_table", dest="hive_table",
                          help="Mode 1: Name of the Hive Table from which data will be exported")
     optparser.add_option("--partition_spec", dest="partition_spec",
