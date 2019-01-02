12852ca08f705807a791e7a6afdf6c8ee135a571
Aditya Karanjkar <akaranjkar@qubole.com>
2019-01-01 23:20:58 -0800

    add api_version to cluster start method

--- qds_sdk/cluster.py
+++ qds_sdk/cluster.py
@@ -95,11 +95,11 @@ class Cluster(Resource):
         return conn.get(cls.element_path(cluster_id_label) + "/state")
 
     @classmethod
-    def start(cls, cluster_id_label):
+    def start(cls, cluster_id_label, api_version=None):
         """
         Start the cluster with id/label `cluster_id_label`.
         """
-        conn = Qubole.agent()
+        conn = Qubole.agent(version=api_version)
         data = {"state": "start"}
         return conn.put(cls.element_path(cluster_id_label) + "/state", data)
 
