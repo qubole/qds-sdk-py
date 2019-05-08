ac1c494dab1c87554d6fbbec5d1bbe43d2ccc327
Joy Lal Chattaraj <Joylal4896@gmail.com>
2019-05-08 11:04:40 +0530

    SDK-350 MultiCloud Fix for Script Location Parameter (#272)

--- tests/test_command.py
+++ tests/test_command.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 import sys
 import os
+import pytest
 if sys.version_info > (2, 7, 0):
     import unittest
 else:
@@ -2052,6 +2053,34 @@ class TestGetResultsCommand(QdsCliTestCase):
 
         with self.assertRaises(SystemExit):
             qds.main()
+
+
+@pytest.mark.parametrize("script_location", [
+    'oci://some_path/file', 'oraclebmc://some_path/file', 'wasb://some_path/file',
+    'gs://some_path/file', 's3://some_path/file', 's3n://some_path/file',
+    's3a://some_path/file', 'swift://some_path/file', 'adl://some_path/file',
+    'abfs://some_path/file', 'abfss://some_path/file'])
+def test_submit_script_location_multi_cloud(script_location):
+    os.environ['QDS_API_TOKEN'] = 'dummy_token'
+    os.environ['QDS_API_URL'] = 'https://qds.api.url/api'
+    sys.argv = ['qds.py', 'hivecmd', 'submit', '--script_location', script_location,
+                '--tags', 'abc,def']
+    print_command()
+    Connection._api_call = Mock(return_value={'id': 1234})
+    qds.main()
+    Connection._api_call.assert_called_with('POST', 'commands',
+            {'macros': None,
+             'hive_version': None,
+             'label': None,
+             'tags': ["abc", "def"],
+             'sample_size': None,
+             'name': None,
+             'query': None,
+             'command_type': 'HiveCommand',
+             'can_notify': False,
+             'script_location': script_location,
+             'retry': 0,
+             'pool': None})
 
 
 if __name__ == '__main__':
