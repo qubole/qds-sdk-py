4694bf32b01e23c762b2b3d3341eb52a2800dfed
Joy Lal Chattaraj <joylal4896@gmail.com>
2019-05-20 17:56:02 +0530

    removed unnecessary if condition

--- qds_sdk/commands.py
+++ qds_sdk/commands.py
@@ -303,19 +303,13 @@ class Command(Resource):
                     boto.config.add_section('s3')
                 boto.config.set('s3', 'use-sigv4', 'True')
                 storage_credentials = conn.get(Account.credentials_rest_entity_path)
-                if storage_credentials['region_endpoint'] is not None:
-                    boto_conn = boto.connect_s3(aws_access_key_id=storage_credentials['storage_access_key'],
-                                                aws_secret_access_key=storage_credentials['storage_secret_key'],
-                                                security_token=storage_credentials['session_token'],
-                                                host=storage_credentials['region_endpoint'])
-                else:
-                    boto_conn = boto.connect_s3(aws_access_key_id=storage_credentials['storage_access_key'],
-                                                aws_secret_access_key=storage_credentials['storage_secret_key'],
-                                                security_token=storage_credentials['session_token'],
-                                                host="s3.amazonaws.com")
-
+                host = storage_credentials['region_endpoint'] if storage_credentials['region_endpoint'] else "s3.amazonaws.com"
+                boto_conn = boto.connect_s3(aws_access_key_id=storage_credentials['storage_access_key'],
+                                            aws_secret_access_key=storage_credentials['storage_secret_key'],
+                                            security_token=storage_credentials['session_token'],
+                                            host=host)
                 log.info("Starting download from result locations: [%s]" % ",".join(r['result_location']))
-                #fetch latest value of num_result_dir
+                # fetch latest value of num_result_dir
                 num_result_dir = Command.find(self.id).num_result_dir
 
                 # If column/header names are not able to fetch then use include header as true
