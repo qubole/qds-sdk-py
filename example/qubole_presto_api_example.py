c009c23ba873cf00c298d5db2b1359e6914933f4
Jean Lescut-Muller <jlescutmuller@expediagroup.com>
2019-07-29 13:13:59 +0000

    Code Improvement + Adding timeout

--- example/qubole_presto_api_example.py
+++ example/qubole_presto_api_example.py
@@ -2,13 +2,13 @@
 This is the sample code used for submitting a Presto query (PrestoCommand) and getting the result back to local file.
 Similar way can be followed for HiveCommand etc.
 """
- 
-import logging, sys, string, time
-from tempfile import NamedTemporaryFile
-from configparser import SafeConfigParser
+
+import logging, sys, time
+#from tempfile import NamedTemporaryFile
+#from configparser import SafeConfigParser
 from qds_sdk.qubole import Qubole
 from qds_sdk.commands import PrestoCommand
-import boto
+#import boto
 import pandas as pd
 
 # Setting up the logger
@@ -22,8 +22,12 @@ log.setLevel(logging.DEBUG)
 #######################
 # PRIVATE FUNCTIONS
 #######################
-def _wait_for_cmd(cmd, update=True):
+def _wait_for_cmd(cmd, update=True, timeout = 3600):
+    start_time = time.time()
     while not PrestoCommand.is_done(cmd.status):
+        if (time.time() - start_time) > timeout:
+            log.error(f"Timeout ({timeout} sec) has been reached! Aborting...")
+            break
         if update: cmd = PrestoCommand.find(cmd.id) # Updating the Python object through HTTP
         time.sleep(3)
     if PrestoCommand.is_success(cmd.status):
@@ -59,21 +63,27 @@ def get_raw_results(cmd, column_names = None):
     with open(_get_results_locally(cmd), 'r') as content_file:
         content = content_file.read()
     return content
- 
+
 def get_dataframe(command, column_names = None, **kwargs):
     assert command is not None
     assert type(command) is PrestoCommand
-    
+
+    if column_names is None and 'dtype' in kwargs.keys():
+        assert type(kwargs['dtype']) is dict
+        column_names = kwargs['dtype'].keys()
+
     _NA_VALUES = list(pd.io.common._NA_VALUES) + ['\\N'] # The NA Values that should be considered for Presto
     filename = _get_results_locally(command)
-    
+
     with open(filename) as f:
         firstline = f.readline()
-    if firstline.strip().split('\t') == column_names :
+    joint_column_names = '\t'.join(column_names)
+    if firstline.strip() == joint_column_names :
         log.debug('It seems that the file already got the right column names...')
         return pd.read_csv(filename, delimiter='\t', na_values=_NA_VALUES, **kwargs)
     else :
         log.debug('It seems that the column names are not present in the file. Adding them :')
-        return pd.read_csv(filename, delimiter='\t', na_values=_NA_VALUES, header=None, names=column_names, **kwargs)
-    
- 
+        log.debug(f"firstline='{firstline.strip()}', joint_column_names={joint_column_names}")
+        kwargs['header'] = None
+        kwargs['names'] = column_names
+        return pd.read_csv(filename, delimiter='\t', na_values=_NA_VALUES, **kwargs)
