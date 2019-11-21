9548474dfd4b597c567407bd535f8c724eb8dfa9
satyavathib <satyavathib@gmail.com>
2019-11-21 13:18:39 +0530

    change

--- tests/test_clusterv2.py
+++ tests/test_clusterv2.py
@@ -457,9 +457,9 @@ class TestClusterCreate(QdsCliTestCase):
                                                     {'engine_config':
                                                          {'flavour': 'hs2'},
                                                     'cluster_info': {'label': ['test_label'],
-                                                    'parent_cluster_id': 1, 
+                                                    'parent-cluster-id': 1, 
                                                     'min_nodes': 3,
-                                                    'node_bootstrap': 'test_file_name',
+                                                    'node_bootstrap': 'test_file_name',
                                                     'slave_instance_type': 'c1.xlarge' }})
 
     def test_spark_engine_config(self):
