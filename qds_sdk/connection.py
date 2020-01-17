3da5844d46e76827ff13e5191eca36e424c4b40e
Saurabh Shekher <sshekher@qubole.com>
2020-01-17 16:39:38 +0530

    nit

--- qds_sdk/connection.py
+++ qds_sdk/connection.py
@@ -76,7 +76,7 @@ class Connection:
     def get_raw(self, path, params=None):
         return self._api_call_raw("GET", path, params=params)
 
-    @retry((RetryWithDelay, requests.Timeout))
+    @retry((RetryWithDelay, requests.Timeout, ServerError))
     def get(self, path, params=None):
         return self._api_call("GET", path, params=params)
 
