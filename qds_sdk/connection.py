2c123e5c485f2a2a9c98154be9e6b5df103814a0
shekharsaurabh <37484772+shekharsaurabh@users.noreply.github.com>
2020-01-30 16:41:34 +0530

    SDK-386: Add Retries and Delay Options for API Rate Limiting (#295)

--- qds_sdk/connection.py
+++ qds_sdk/connection.py
@@ -3,6 +3,7 @@ import requests
 import logging
 import ssl
 import json
+import time
 import pkg_resources
 from requests.adapters import HTTPAdapter
 from datetime import datetime
@@ -10,8 +11,8 @@ try:
     from requests.packages.urllib3.poolmanager import PoolManager
 except ImportError:
     from urllib3.poolmanager import PoolManager
-from qds_sdk.retry import retry
 from qds_sdk.exception import *
+from functools import wraps
 
 
 log = logging.getLogger("qds_connection")
@@ -34,7 +35,9 @@ class MyAdapter(HTTPAdapter):
 
 class Connection:
 
-    def __init__(self, auth, rest_url, skip_ssl_cert_check, reuse=True):
+    def __init__(self, auth, rest_url, skip_ssl_cert_check,
+                 reuse=True, max_retries=5,
+                 base_retry_delay=10):
         self.auth = auth
         self.rest_url = rest_url
         self.skip_ssl_cert_check = skip_ssl_cert_check
@@ -42,6 +45,8 @@ class Connection:
                          'Content-Type': 'application/json'}
 
         self.reuse = reuse
+        self.max_retries = max_retries
+        self.base_retry_delay = base_retry_delay
         if reuse:
             self.session = requests.Session()
             self.session.mount('https://', MyAdapter())
@@ -50,20 +55,46 @@ class Connection:
             self.session_with_retries = requests.Session()
             self.session_with_retries.mount('https://', MyAdapter(max_retries=3))
 
-    @retry((RetryWithDelay, requests.Timeout, ServerError), tries=6, delay=30, backoff=2)
+    def retry(ExceptionToCheck, tries=5, delay=10, backoff=2):
+        def deco_retry(f):
+            @wraps(f)
+            def f_retry(self, *args, **kwargs):
+                if hasattr(self, 'max_retries'):
+                    mtries, mdelay = self.max_retries, self.base_retry_delay
+                else:
+                    mtries, mdelay = tries, delay
+                while mtries >= 1:
+                    try:
+                        return f(self, *args, **kwargs)
+                    except ExceptionToCheck as e:
+                        logger = logging.getLogger("retry")
+                        msg = "%s, Retrying in %d seconds..." % (e.__class__.__name__,
+                                                                 mdelay)
+                        logger.info(msg)
+                        time.sleep(mdelay)
+                        mtries -= 1
+                        mdelay *= backoff
+                return f(self, *args, **kwargs)
+            return f_retry  # true decorator
+        return deco_retry
+
+    @retry((RetryWithDelay, requests.Timeout))
     def get_raw(self, path, params=None):
         return self._api_call_raw("GET", path, params=params)
 
-    @retry((RetryWithDelay, requests.Timeout, ServerError), tries=6, delay=30, backoff=2)
+    @retry((RetryWithDelay, requests.Timeout, ServerError))
     def get(self, path, params=None):
         return self._api_call("GET", path, params=params)
 
+    @retry((RetryWithDelay, requests.Timeout))
     def put(self, path, data=None):
         return self._api_call("PUT", path, data)
 
+    @retry((RetryWithDelay, requests.Timeout))
     def post(self, path, data=None):
         return self._api_call("POST", path, data)
 
+    @retry((RetryWithDelay, requests.Timeout))
     def delete(self, path, data=None):
         return self._api_call("DELETE", path, data)
 
@@ -111,10 +142,8 @@ class Connection:
     @staticmethod
     def _handle_error(response):
         """Raise exceptions in response to any http errors
-
         Args:
             response: A Response object
-
         Raises:
             BadRequest: if HTTP error code 400 returned.
             UnauthorizedAccess: if HTTP error code 401 returned.
@@ -134,8 +163,8 @@ class Connection:
         
         if 'X-Qubole-Trace-Id' in response.headers:
             now = datetime.now()
-            time = now.strftime('%Y-%m-%d %H:%M:%S')
-            format_list = [time,response.headers['X-Qubole-Trace-Id']]
+            time_now = now.strftime('%Y-%m-%d %H:%M:%S')
+            format_list = [time_now, response.headers['X-Qubole-Trace-Id']]
             sys.stderr.write("[{}] Request ID is: {}. Please share it with Qubole Support team for any assistance".format(*format_list) + "\n")
             
         if code == 400:
@@ -164,7 +193,10 @@ class Connection:
             raise RetryWithDelay(response)
         elif code == 449:
             sys.stderr.write(response.text + "\n")
-            raise RetryWithDelay(response, "Data requested is unavailable. Retrying ...")
+            raise RetryWithDelay(response, "Data requested is unavailable. Retrying...")
+        elif code == 429:
+            sys.stderr.write(response.text + "\n")
+            raise RetryWithDelay(response, "Too many requests. Retrying...")
         elif 401 <= code < 500:
             sys.stderr.write(response.text + "\n")
             raise ClientError(response)
