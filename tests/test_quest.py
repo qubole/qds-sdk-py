3a94cfadffd4cb289114c3c68d2e5d8a22304ef1
harshits <harshits@qubole.com>
2020-02-07 17:40:04 +0530

    Added changes in List cli test

--- tests/test_quest.py
+++ tests/test_quest.py
@@ -8,17 +8,18 @@ else:
     import unittest2 as unittest
 from mock import *
 
-sys.path.append(os.path.join(os.path.dirname(__file__), '../bin'))
 import qds
 from qds_sdk.connection import Connection
 from qds_sdk.quest import QuestCode, QuestJar
 from test_base import print_command
 from test_base import QdsCliTestCase
 
+sys.path.append(os.path.join(os.path.dirname(__file__), '../bin'))
+
 
 class TestQuestList(QdsCliTestCase):
     def test_list_pipeline(self):
-        sys.argv = ['qds.py', 'quest', 'status', '--pipeline-status', 'all']
+        sys.argv = ['qds.py', 'quest', 'list', '--pipeline-status', 'all']
         print_command()
         Connection._api_call = Mock(return_value={})
         params = {'status': "all"}
