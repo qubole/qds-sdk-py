3d0bc69e4ce2d235c4a401decf97d044076e5a9d
harshits <harshits@qubole.com>
2020-02-10 19:23:08 +0530

    added test for create pipeline of create type=3

--- tests/test_quest.py
+++ tests/test_quest.py
@@ -53,3 +53,23 @@ class TestQuestList(QdsCliTestCase):
         Connection._api_call = Mock(return_value={})
         qds.main()
         Connection._api_call.assert_called_with("PUT", "pipelines/153/delete", None)
+
+    def test_create_pipeline(self):
+        sys.argv = ['qds.py', 'quest', 'create', '--pipeline-id', '153']
+        print_command()
+        Connection._api_call = Mock(return_value={})
+        qds.main()
+        d1 = {"data": {"attributes": {"name": "test_pipeline_name", "status": "DRAFT", "create_type": "3"},
+                       "type": "pipelines"}}
+        d2 = {"data": {"attributes": {"cluster_label": "cluster_label", "can_retry": "can_retry",
+                                      "checkpoint_location": "checkpoint_location",
+                                      "trigger_interval": "trigger_interval", "output_mode": "output_mode",
+                                      "command_line_options": "command_line_options"}, "type": "pipeline/properties"}}
+        d3 = {"data": {"attributes": {"create_type": "3", "user_arguments": "--user arguments", "code": "hello world",
+                                      "language": "python"}}}
+        c1 = {"req_type": "POST", "path": "pipelines?mode=wizard", "data": d1}
+        c2 = {"req_type": "PUT", "path": "pipelines/1/properties", "data": d2}
+        c3 = {"req_type": "PUT", "path": "pipelines/1/save_code", "data": d3}
+        calls = [c1, c2, c3]
+        qds.main()
+        Connection._api_call.assert_has_calls(calls)
