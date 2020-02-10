524c707a5703a213eebebd8e56ca54986bd571cf
harshits <harshits@qubole.com>
2020-02-10 19:34:37 +0530

    added test for create pipeline of create type=3

--- tests/test_quest.py
+++ tests/test_quest.py
@@ -55,18 +55,56 @@ class TestQuestList(QdsCliTestCase):
         Connection._api_call.assert_called_with("PUT", "pipelines/153/delete", None)
 
     def test_create_pipeline(self):
-        sys.argv = ['qds.py', 'quest', 'create', '--create-type', '3', '--pipeline-name', 'test_pipeline_name', '--cluster-label', 'spark', '-c', 'print("hello")', '--language', 'python']
+        sys.argv = ['qds.py', 'quest', 'create', '--create-type', '3', '--pipeline-name', 'test_pipeline_name',
+                    '--cluster-label', 'spark', '-c', 'print("hello")', '--language', 'python']
         print_command()
-        Connection._api_call = Mock(return_value={})
         qds.main()
         d1 = {"data": {"attributes": {"name": "test_pipeline_name", "status": "DRAFT", "create_type": "3"},
                        "type": "pipelines"}}
+        r1 = {
+            "relationships": {
+                "nodes": [],
+                "alerts": []
+            },
+            "included": [],
+            "meta": {
+                "command_details": {
+                    "code": """print("hello")""",
+                    "language": "python"
+                },
+                "properties": {
+                    "checkpoint_location": None,
+                    "cluster_label": "spark",
+                    "is_monitoring_enabled": None
+                },
+                "query_hist": None,
+                "cluster_id": None
+            },
+            "data": {
+                "id": 585,
+                "type": "pipeline",
+                "attributes": {
+                    "name": "test_pipeline_name",
+                    "description": None,
+                    "status": "draft",
+                    "created_at": "2020-02-10T14:02:20Z",
+                    "updated_at": "2020-02-10T14:02:20Z",
+                    "cluster_label": None,
+                    "owner_name": "eam-airflow",
+                    "pipeline_instance_status": "draft",
+                    "create_type": 3,
+                    "health": "UNKNOWN"
+                }
+            }
+        }
+        Connection._api_call = Mock(return_value=r1)
         d2 = {"data": {"attributes": {"cluster_label": "spark", "can_retry": "can_retry",
                                       "checkpoint_location": "checkpoint_location",
                                       "trigger_interval": "trigger_interval", "output_mode": "output_mode",
                                       "command_line_options": "command_line_options"}, "type": "pipeline/properties"}}
-        d3 = {"data": {"attributes": {"create_type": "3", "user_arguments": "--user arguments", "code": """"print("hello")""",
-                                      "language": "python"}}}
+        d3 = {"data": {
+            "attributes": {"create_type": "3", "user_arguments": "--user arguments", "code": """"print("hello")""",
+                           "language": "python"}}}
         c1 = {"req_type": "POST", "path": "pipelines?mode=wizard", "data": d1}
         c2 = {"req_type": "PUT", "path": "pipelines/1/properties", "data": d2}
         c3 = {"req_type": "PUT", "path": "pipelines/1/save_code", "data": d3}
