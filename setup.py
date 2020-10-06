399a671258c49e17088fbe25db499053375b705d
Joy Lal Chattaraj <8450903+chattarajoy@users.noreply.github.com>
2020-10-06 16:00:33 +0530

    Release Version 1.16.1

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ def read(fname):
 
 setup(
     name="qds_sdk",
-    version="1.16.0",
+    version="1.16.1",
     author="Qubole",
     author_email="dev@qubole.com",
     description=("Python SDK for coding to the Qubole Data Service API"),
